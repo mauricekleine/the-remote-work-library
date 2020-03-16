6f5acb258b619de5bc7e11ae4d15b7cdaeae043a
Maurice Kleine <info@mauricekleine.com>
2020-03-16 16:40:24 +0100

    fixed next config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./(components|pages)/**/*.html", "./(components|pages)/**/*.js"],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: process.env.NODE_ENV === "production" ? {} : false,
+    "@fullhuman/postcss-purgecss":
+      process.env.NODE_ENV === "production"
+        ? {
+            content: [
+              "./(components|pages)/**/*.html",
+              "./(components|pages)/**/*.js"
+            ],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        : false
+  }
 };
