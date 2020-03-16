41cfb55c0c9d507f77e54752674b35745bda0d17
Maurice Kleine <info@mauricekleine.com>
2020-03-16 13:51:05 +0100

    improve SEO by adding sitemap and robots.txt

--- next.config.js
+++ next.config.js
@@ -4,5 +4,12 @@ module.exports = {
   env: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
     AIRTABLE_ENDPOINT_URL: process.env.AIRTABLE_ENDPOINT_URL
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./utils/generate-sitemap");
+    }
+
+    return config;
   }
 };
