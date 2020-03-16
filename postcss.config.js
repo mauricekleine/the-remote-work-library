54303f0869bf2a2e87ccf52329ed9000cd3d56b2
Maurice Kleine <info@mauricekleine.com>
2020-03-16 15:56:01 +0100

    purge unused css in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./(components|pages)/**/*.html", "./(components|pages)/**/*.js"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: {
-    tailwindcss: {}
-  }
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
