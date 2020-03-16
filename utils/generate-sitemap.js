4262de5ea2648ed4b2105b7a4d91ef5a96f526e2
Maurice Kleine <info@mauricekleine.com>
2020-03-16 14:05:29 +0100

    create dynamic sitemap

--- utils/generate-sitemap.js
+++ utils/generate-sitemap.js
@@ -1,31 +1,38 @@
 const fs = require("fs");
-const globby = require("globby");
 const prettier = require("prettier");
 
+const { getCompanies, getResources } = require("./fetch");
+
 (async () => {
-  // Ignore Next.js specific files (e.g., _app.js)
-  const pages = await globby(["pages/**/*{.js,.mdx}", "!pages/_*.js"]);
+  const companies = await getCompanies();
+  const resources = await getResources();
+
+  const companiesRoutes = companies.map(
+    ({ name }) => `resources/${name.toLowerCase().replace(/ /g, "")}`
+  );
+
+  const tagsRoutes = [
+    ...new Set(resources.map(({ type }) => `tags/${type.toLowerCase()}`))
+  ];
 
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map(page => {
-                const path = page
-                  .replace("pages", "")
-                  .replace(".js", "")
-                  .replace(".mdx", "");
-                const route = path === "/index" ? "" : path;
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://theremoteworklibrary.com</loc>
+      </url>
 
-                return `
-                        <url>
-                            <loc>${`https://theremoteworklibrary.com${route}`}</loc>
-                        </url>
-                    `;
-              })
-              .join("")}
-        </urlset>
-    `;
+      ${[...companiesRoutes, ...tagsRoutes]
+        .map(
+          route => `
+            <url>
+              <loc>${`https://theremoteworklibrary.com/${route}`}</loc>
+            </url>
+          `
+        )
+        .join("")}
+    </urlset>
+  `;
 
   const formatted = prettier.format(sitemap, {
     parser: "html"
